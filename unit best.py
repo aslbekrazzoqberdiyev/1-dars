a2a2c957142021ee75f012e17f4315d8e255c29b
aslbekrazzoqberdiyev <116031611+aslbekrazzoqberdiyev@users.noreply.github.com>
2022-10-17 22:25:42 +0300

    Add files via upload

---
+++ unit best.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 11 20:19:48 2022
+
+@author: user
+"""
+#mehmonlar = ['Ali','Vali','Hasan','Husan','Olim']
+#for mehmon in mehmonlar:
+    #print(f"hurmatli {mehmon},")
+    #print("sizni 20-mart kuni nahorgi oshga taklif qilamz.")
+    #print('hurmat bilan , palonchiyevlar oilsai')
+#cars = ['nexica','tico','damas']
+#for car in cars:
+    #print(car.title())
+#print('korganlar qilar havas')  
+cars = ['nexica','tico','damas']
+for car in cars:
+    print(car.title())
+    print('korganlar qilar havas')
+
+
+    
